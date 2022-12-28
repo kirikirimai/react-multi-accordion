3abd977866eb39e9f1bd86db193f304b14116c7c
kirikirimai <go.revolution.no3@gmail.com>
2022-12-28 21:10:53 +0900

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <h1>Framer Motionを利用したアコーディオン</h1>
       <p>下記の条件でアコーディオン</p> 
       <ul> 
+        <li>JSONファイルからデータを取ってくる</li>
         <li>最初のアコーディオンは開いている</li>
         <li>他のアコーディオンをクリックししたら開いてるアコーディオンは閉じる</li>
         <li>開いているアコーディオンをクリックしたらそのアコーディオンが閉じる</li>
       </ul>
+      <hr />
+
+      <div className="accBlock">
+        <div className="accBlock__btn">
+          アコーディオンタイトル
+        </div>
+        <div className="accBlock__body">
+          <p>アコーディオンの内容</p>
+        </div>
+        <div className="accBlock__btn">
+          アコーディオンタイトル
+        </div>
+        <div className="accBlock__body">
+          <p>アコーディオンの内容</p>
+        </div>
+      </div>
     </div>
   )
 }
